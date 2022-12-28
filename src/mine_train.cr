745fb0bd40251f03acdb3592ce3380fedf3d4932
SteffanPerry <sperry1988@gmail.com>
2022-12-28 09:57:26 -0500

    move run method

--- src/mine_train.cr
+++ src/mine_train.cr
@@ -34,3 +34,23 @@ module MineTrain
     HTTP::Client.post("#{INVOCATION_URL}/init/error", body: data, headers: nil)
   end
 end
+
+def mine_train_run
+  while true
+    begin
+      fetched_event = MineTrain.fetch_event
+      event         = MineTrain::Lambda::Event.new(fetched_event)
+      context       = MineTrain::Lambda::Context.new(fetched_event)
+
+      result = yield(event, context)
+
+      MineTrain.send_response(fetched_event, result)
+    rescue ex
+      return if fetched_event.nil?
+
+      MineTrain.send_error_response(fetched_event, ex)
+    end
+  end
+rescue ex
+  MineTrain.send_initialization_error_response(ex)
+end
